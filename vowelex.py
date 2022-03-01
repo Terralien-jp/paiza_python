bca6e4498d32974ad554ba7683283c4d8bc68aa5
Terralien <95989596+Terralien-jp@users.noreply.github.com>
2022-03-01 22:51:43 +0900

    Update vowelex.py

--- vowelex.py
+++ vowelex.py
@@ -6,3 +6,9 @@ name = input()
 #'間に挿入する文字列'.join([連結したい文字列のリスト])
 cname = ''.join(s for s in name if s not in vowel)
 print(cname)
+
+#複数行の入出力
+input_line = int(input())
+for i in range(input_line):
+    n = input()
+    print(n)
